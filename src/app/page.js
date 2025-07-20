96e4e42441a1d4fea40695c6ebfb6c4172f9ed32
Jayedjb765 <jayedjb76@gmail.com>
2025-07-20 23:47:56 +0600

    routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const HomePage = () => {
+  const router = useRouter();
+  const handelNavigate = () => {
+    router.push('/dashboard')
+  }
   return (
     <div>
       <h1 className="text-center">Home Page</h1>
       <h1 className="text-2xl">This is Home page</h1>
+
+
+      <button onClick={handelNavigate}>Dashboard</button>
+
     </div>
   );
 };
