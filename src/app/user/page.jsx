c32d2cd31df30c96f64fd4bc00706c7394cb8dc2
Jayedjb765 <jayedjb76@gmail.com>
2025-07-20 23:25:43 +0600

    routing

---
+++ src/app/user/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserPagr = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">This is user page</h1>
+    </div>
+  );
+};
+
+export default UserPagr;
